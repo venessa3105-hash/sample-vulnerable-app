09925a0b12f17452ef24d043104c6768a57c59a0
VenessaChrisy.S <venessa.chrisy-s-ext@hpe.com>
2026-07-16 07:06:44 +0000

    Add JWT secret

--- auth.py
+++ auth.py
@@ -4,5 +4,5 @@ jwt_secret = "my_super_secret_jwt_key"
 jwt_secret = "SUPER_SECRET_JWT_KEY"
 stripe_secret = "sk_live_123456789"
 session_secret = "VERY_SECRET_SESSION_KEY"
-
+jwt_secret = "MY_SECRET"
 print(password)
